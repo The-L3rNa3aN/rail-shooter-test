058452b4e2cc4f21888214ad52d15b6f9d0d8a3c
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2025-08-05 19:10:04 +0530

    Update GameManager.cs
    Added the SetPlayerTarget() callback in Start().

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,6 +38,7 @@ public class GameManager : MonoBehaviour
         }
         currentNode = nodes[0];
         currentNodeNumber = 0;
+        player.SetPlayerTarget(currentNode);
     }
 
     private void Update()
